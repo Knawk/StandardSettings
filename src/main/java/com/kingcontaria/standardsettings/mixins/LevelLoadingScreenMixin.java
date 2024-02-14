3a7b8d8f62185fef119bfd91adb8afcb293afabf
tildejustin <spacepepper3.14@gmail.com>
2024-02-14 17:15:41 -0500

    for worldpreview, only attempt the pause once, right as the preview starts rendering

--- src/main/java/com/kingcontaria/standardsettings/mixins/LevelLoadingScreenMixin.java
+++ src/main/java/com/kingcontaria/standardsettings/mixins/LevelLoadingScreenMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.screen.LevelLoadingScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,6 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LevelLoadingScreen.class)
 public abstract class LevelLoadingScreenMixin extends Screen {
     // For the WorldPreview to get paused
+    @Unique
+    private boolean previewPauseDone = false;
 
     protected LevelLoadingScreenMixin(Text title) {
         super(title);
@@ -20,8 +23,9 @@ public abstract class LevelLoadingScreenMixin extends Screen {
 
     @Inject(method = "render", at = @At("HEAD"))
     public void standardSettings_render(int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        if (StandardSettings.hasWP) {
-            IfWPExists.handleLevelLoadScreenRender(minecraft);
+
+        if (StandardSettings.hasWP && !this.previewPauseDone) {
+            this.previewPauseDone = IfWPExists.handleLevelLoadScreenRender(minecraft);
         }
     }
 
