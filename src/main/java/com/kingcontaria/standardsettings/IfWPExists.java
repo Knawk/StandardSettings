c93b79aded73070ecbc22606027baebbaf761ae2
andrew <cowslayer6789@gmail.com>
2024-03-26 18:26:37 -0400

    remove focus window check for WorldPreview bump version

--- src/main/java/com/kingcontaria/standardsettings/IfWPExists.java
+++ src/main/java/com/kingcontaria/standardsettings/IfWPExists.java
@@ -1,15 +1,16 @@
 package com.kingcontaria.standardsettings;
 
 import me.voidxwalker.worldpreview.WorldPreview;
-import net.minecraft.client.MinecraftClient;
 
 public class IfWPExists {
-    public static boolean handleLevelLoadScreenRender(MinecraftClient minecraft) {
+    public static boolean handleLevelLoadScreenRender() {
         if (StandardSettings.f3PauseOnWorldLoad && StandardSettings.hasWP) {
-            if (!minecraft.isWindowFocused() && WorldPreview.renderingPreview) {
+            if (WorldPreview.inPreview) {
                 WorldPreview.showMenu = false;
                 return true;
-            } else return WorldPreview.renderingPreview;
+            } else {
+                return false;
+            }
         }
         return true;
     }
